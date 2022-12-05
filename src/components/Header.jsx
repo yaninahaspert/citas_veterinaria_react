f9a54fc34631b9ae8c797b6db3d312663f008bc4
yaninahaspert <haspertyanina@gmailcom>
2022-12-05 13:46:01 -0300

    x

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">Seguimiento pacientes {" "}
-            <span className="text-red-500-600">Veterinaria</span></h1>
+            <span className="text-indigo-500-600">Veterinaria</span></h1>
 
     )
 }
